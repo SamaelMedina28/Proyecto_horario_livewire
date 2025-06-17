acc2033c945960ef4c2a1f6e4eb0a0ba01980a05
Samael Home <samaelortiz2218@gmail.com>
2025-06-17 12:09:33 -0700

    Pequeño funcionamiento de mostrar-materias

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,35 @@ use App\Http\Controllers\config\MateriasController;
 use App\Models\Clase;
 
 Route::get('/', function () {
-    $clases = auth()->user()->clases()->where('dia', 'domingo')->get();
+    //Obtener el dia actual
+    // Obtener el día en inglés
+    $diaIngles = strtolower(date('l')); // Ej: "monday"
+
+    // Mapeo de días inglés -> español
+    $diasTraducidos = [
+        'monday'    => 'lunes',
+        'tuesday'   => 'martes',
+        'wednesday' => 'miercoles',
+        'thursday'  => 'jueves',
+        'friday'    => 'viernes',
+        'saturday'  => 'sabado',
+        'sunday'    => 'domingo'
+    ];
+
+    // Obtener el día en español
+    $diaActual = $diasTraducidos[$diaIngles];
+
+    // Consulta
+    $clases = auth()->user()->clases()->where('dia', $diaActual)->get();
     return $clases;
 })->middleware('auth', 'isOld')->name('welcome');
 
 Route::get('/materias', [MateriasController::class, 'index'])->middleware('auth', 'isNew')->name('materias.index');
+Route::get('/{dia}', function ($dia) {
+    $clases = auth()->user()->clases()->where('dia', $dia)->get();
+    return $clases;
+})->middleware('auth', 'isOld')->name('clases');
+
 
 Route::get('/clases/{dia}', [MateriasController::class, 'clases'])->middleware('auth', 'isNew')->name('materias.clases');
 
