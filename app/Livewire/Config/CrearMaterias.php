f35f8d21fec29bc04eb2030e1c165200e9f71e41
Samael Home <samaelortiz2218@gmail.com>
2025-06-18 16:33:38 -0700

    Se corrigio un bug por el camino que hacia que a veces no redirigiera a la siguiente pagina ( agregar clases)

--- app/Livewire/Config/CrearMaterias.php
+++ app/Livewire/Config/CrearMaterias.php
@@ -38,7 +38,6 @@ class CrearMaterias extends Component
             Materia::create($formulario);
         }
 
-        $this->formularios = [['nombre' => '', 'user_id' => auth()->user()->id]];
         //redirigir a la vista de materias sin recargar la pagina
         return $this->redirect(route('materias.clases', ['dia' => 'Lunes']), navigate: true);
     }
