91abd4c7857bffdc16b368cb1e81792bacaae215
Samael Home <samaelortiz2218@gmail.com>
2025-06-18 12:35:40 -0700

    Creacion de todas las rutas y vista inicial

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,25 @@ use App\Http\Controllers\config\MateriasController;
 use App\Http\Controllers\config\ClasesController;
 
 
-// ? Rutas para mostrar todas las clases
-Route::get('/', [ClasesController::class, 'index'])->middleware('auth', 'isOld')->name('welcome');
-Route::get('/clase/{id}', [ClasesController::class, 'show'])->middleware('auth', 'isOld')->name('clases.show');
+// Rutas con middleware 'auth' y 'isOld'
+Route::middleware(['auth', 'isOld'])->group(function () {
+    // ? Rutas para mostrar todas las clases
+    Route::get('/', [ClasesController::class, 'index'])->name('welcome');
+    Route::get('/clase/{id}', [ClasesController::class, 'show'])->name('clases.show');
+});
 
-// ? Rutas para crear
-Route::get('/config/materias', [MateriasController::class, 'index'])->middleware('auth', 'isNew')->name('materias.index');
-Route::get('/config/clases/{dia}', [MateriasController::class, 'clases'])->middleware('auth', 'isNew')->name('materias.clases');
+// Rutas con middleware 'auth' y 'isNew'
+Route::middleware(['auth', 'isNew'])->group(function () {
+    // ? Rutas para crear
+    Route::get('/config/materias', [MateriasController::class, 'index'])->name('materias.index');
+    Route::get('/config/clases/{dia}', [MateriasController::class, 'clases'])->name('materias.clases');
+});
 
-
-Route::middleware([
+/* Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+    'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
-    })->name('dashboard');
-});
+    })->name('dashboard');});
+ */
